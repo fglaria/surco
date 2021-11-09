f9f5df1229c1ce7632fd8ed51fc1d19d92f249ab
Felipe Glaría Grego <fglaria@gmail.com>
2021-11-09 19:34:36 -0300

    client added

--- models/users.js
+++ models/users.js
@@ -1,18 +1,23 @@
 var mongoose = require('mongoose');
 
-var Users = new mongoose.Schema({
+var User = new mongoose.Schema({
   id: { type: Number, required: true},
   nickname: {type: String, required: true },
   names: { type: String, required: true },
   lastname1: { type: String, required: true },
   lastname2: { type: String, required: true },
+  tutor: { type: String, required: false },
   email: { type: String, required: true },
   password: { type: String, required: true },
   address: { type: String, required: false },
   commune: { type: String, required: false },
   phone: { type: String, required: true },
   roles: [
-    { type: String, lesson: String, since: Date }
+    [ 
+      { type: String, required: true },
+      { courses: String, required: false },
+      { since: Date, required: true}
+    ],
   ],
   created_at : { type: Date, default: Date.now },
   modified_at : { type: Date },
